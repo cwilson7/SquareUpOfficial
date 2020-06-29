1f4dbbbda9f5f59dc5946829b61d57a155be2811
Chase <cwilson7@trinity.edu>
2020-06-29 15:15:56 -0700

    spawning stuff

--- Assets/Scripts/Level.cs
+++ Assets/Scripts/Level.cs
@@ -4,10 +4,27 @@ using UnityEngine;
 
 public class Level : MonoBehaviour
 {
-    public Transform[] spawnPoints, powerUpSpawnPoints;
+    [HideInInspector] public Transform[] spawnPoints, powerUpSpawnPoints;
     public Transform face;
     public int num;
     public float GravityMultiplier;
+
+    private void Start()
+    {
+        spawnPoints = ArrayOfChildren(transform.FindChild("PlayerSpawnPoints"));
+        powerUpSpawnPoints = ArrayOfChildren(transform.FindChild("PowerUpSpawnPoints"));
+    }
+
+    private Transform[] ArrayOfChildren(Transform kid)
+    {
+        List<Transform> retList = new List<Transform>();
+        for (int i = 0; i < kid.childCount; i++)
+        {
+            retList.Add(kid.GetChild(i));
+        }
+        Transform[] retArray = retList.ToArray();
+        return retArray;
+    }
 
     //DEPRECATED
     public Transform[] ReturnArrayFromID(int id)

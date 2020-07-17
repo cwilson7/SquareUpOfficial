f7e7d930ee078020e26ea7653bf376b25862774b
Chase Wilson <cwilson7@trinity.edu>
2020-07-17 13:59:22 -0500

    punch and gun

--- Assets/Scripts/Fist.cs
+++ Assets/Scripts/Fist.cs
@@ -10,24 +10,32 @@ public class Fist : DamageDealer
     SphereCollider collide;
     SpringJoint spring;
     Rigidbody rb;
+    public float restSpringForce;
+    public float punchSpringForce;
+    public float punchForce;
 
     public Vector3 startLoc;
+
+    public bool hasGun;
 
     private void Update()
     {
         if (ParentController == null || !ParentController.controllerInitialized) return;
         if (collide.enabled == false) startLoc = transform.position;
+        if (hasGun) trackMouse(ParentController.AimDirection);
+
     }
 
     public void InitializeFist(Controller parentController)
     {
         ParentController = parentController;
         collide = GetComponent<SphereCollider>();
-        spring = GetComponent<SpringJoint>();
+        spring = transform.parent.GetComponent<SpringJoint>();
         rb = GetComponent<Rigidbody>();
-        spring.connectedBody = parentController.gameObject.GetComponent<Rigidbody>();
-        if (transform.parent.GetComponent<LFist>() == null) spring.connectedAnchor = new Vector3(1.2f, 2f, -.55f);
-        else spring.connectedAnchor = new Vector3(-1.2f, 2f, -.55f);
+        spring.connectedBody = rb;
+        //if (transform.parent.GetComponent<LFist>() == null) spring.anchor = new Vector3(1.2f, 2f, -.55f);
+        //else spring.anchor = new Vector3(-1.2f, 2f, -.55f);
+        //spring.connectedAnchor = new Vector3(0, 0, -0.015f);
         //spring.connectedAnchor = gameObject.transform.position;
         SetCollider(false);
 
@@ -36,11 +44,51 @@ public class Fist : DamageDealer
         owner = ParentController.actorNr;
         startLoc = transform.position;
         gameObject.tag = "Fist";
+        restSpringForce = 50;
+        punchSpringForce = 5;
+        punchForce = 75;
+        spring.spring = restSpringForce;
+        hasGun = false;
     }
 
     public void Punch(Vector3 aim)
     {
+        spring.spring = punchSpringForce;
+        rb.useGravity = false;
+        SetCollider(true);
+        rb.AddForce(new Vector3(aim.x, aim.y,0f) * punchForce);
+        StartCoroutine(FistDrag());
+    }
 
+    IEnumerator FistDrag()
+    {
+        yield return new WaitForSeconds(0.4f);
+        rb.useGravity = true;
+        spring.spring = restSpringForce;
+        //rb.velocity = new Vector3(0, 0, 0);
+        //transform.localPosition = spring.connectedAnchor;
+        SetCollider(false);
+
+    }
+
+    public void SetHasGun(bool gun)
+    {
+        hasGun = gun;
+        if (gun)
+        {
+            rb.useGravity = false;
+            spring.spring = 0f;
+        }
+        else
+        {
+            rb.useGravity = true;
+            spring.spring = restSpringForce;
+        }
+    }
+
+    private void trackMouse(Vector3 aimDir)
+    {
+        rb.position = aimDir*2;
     }
 
     public void SetCollider(bool isActive) { collide.enabled = isActive; }
--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -148,11 +148,12 @@ public class GameManager : MonoBehaviour
         Score playerInfo = (Score)GameInfo.GI.scoreTable[actor];
         //GameObject wpn = Instantiate(Resources.Load<GameObject>("PhotonPrefabs/Weapons/" + weaponName), playerInfo.playerAvatar.GetComponentInChildren<GunPivot>().transform.position, Quaternion.identity);
         GameObject wpn = Instantiate(Resources.Load<GameObject>("PhotonPrefabs/Weapons/" + weaponName), playerInfo.playerAvatar.GetComponentInChildren<GunLocation>().transform.position, Quaternion.identity);
-        //wpn.transform.SetParent(playerInfo.playerAvatar.GetComponentInChildren<GunPivot>().transform);
-        wpn.transform.SetParent(playerInfo.playerAvatar.transform);
+        wpn.transform.SetParent(playerInfo.playerAvatar.GetComponentInChildren<GunLocation>().transform);
+        //wpn.transform.SetParent(playerInfo.playerAvatar.transform);
         wpn.GetComponent<Weapon>().owner = actor;
         wpn.GetComponent<Weapon>().GunLocation = playerInfo.playerAvatar.GetComponentInChildren<GunLocation>().transform;
         playerInfo.playerAvatar.GetComponent<Controller>().currentWeapon = wpn.GetComponent<Weapon>();
+        playerInfo.playerAvatar.GetComponent<Controller>().EquipWeapon();
         //wpn.transform.rotation = new Vector3()
     }
 

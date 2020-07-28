1a370bf95f1a0df7b5e2cd7195c04ff66aa56dcf
Chase Wilson <cwilson7@trinity.edu>
2020-07-27 22:20:07 -0500

    added characters and fists and cosmetics

---
+++ Assets/Resources/Classes/StickyController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StickyController : Controller
+{
+    public override void InitializePlayerController()
+    {
+        base.InitializePlayerController();
+        audioKey = "Dash";
+        audioHandler.InitializeAudio(audioKey);
+    }
+
+    public override void SpecialAbility()
+    {
+
+    }
+}
---
+++ Assets/Resources/Classes/WarpController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WarpController : Controller
+{
+    public override void InitializePlayerController()
+    {
+        base.InitializePlayerController();
+        audioKey = "Dash";
+        audioHandler.InitializeAudio(audioKey);
+    }
+
+    public override void SpecialAbility()
+    {
+
+    }
+}

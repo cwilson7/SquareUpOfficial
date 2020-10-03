faafc66cd8017aa5916560c529637d2868e8e1a8
chase <thechasewilson1@gmail.com>
2020-10-03 14:50:38 -0700

    fixed gun rotation

--- Assets/Scripts/Game/Weapon.cs
+++ Assets/Scripts/Game/Weapon.cs
@@ -8,6 +8,7 @@ public class Weapon : MonoBehaviour
 {
     private PhotonView PV;
     private Controller ParentController;
+    public GameObject parentAvatar;
     private AnimationSynchronization AS;
 
     public float damage, fireRate, fireCooldown, bltSpeed, recoil;
@@ -19,9 +20,9 @@ public class Weapon : MonoBehaviour
 
     public Vector3 networkedRotation;
 
-    private void Start()
+    public void InitalizeWeapon()
     {
-        GameObject parentAvatar = Utils.FindParentWithClass<Controller>(transform).gameObject;
+        parentAvatar = ((Score)GameInfo.GI.scoreTable[owner]).playerAvatar;
         PV = parentAvatar.GetComponent<PhotonView>();
         ParentController = parentAvatar.GetComponent<Controller>();
         AS = ParentController.GetComponentInChildren<AnimationSynchronization>();
@@ -42,45 +43,28 @@ public class Weapon : MonoBehaviour
 
     private void NetworkTracking()
     {
-        //recieve info from controller and set 
         networkedRotation = ParentController.GetComponent<NetworkAvatar>().netAim;
-        Quaternion desiredRotation = Quaternion.Euler(networkedRotation.x, networkedRotation.y, networkedRotation.z);
-        float angle = Quaternion.Angle(transform.localRotation, desiredRotation);
-        transform.localRotation = Quaternion.Euler(Vector3.Slerp(transform.localRotation.eulerAngles, desiredRotation.eulerAngles, angle * (1.0f / PhotonNetwork.SerializationRate)));
-        //works but has jittery rotation >> Vector3.Slerp(transform.localRotation.eulerAngles, desiredRotation.eulerAngles, angle * (1.0f / PhotonNetwork.SerializationRate)));
-        //rotates around weird angle >> Quaternion.RotateTowards(Quaternion.Euler(transform.localRotation.eulerAngles), desiredRotation, angle * (1.0f / PhotonNetwork.SerializationRate));
+        transform.position = GunPivot.position;
+        NetSetTransform(networkedRotation);
     }
 
     public void TrackMousePosition()//Vector3 Direction, bool directionCheck, bool lerp)
     {
-        bool lerp = false;
-        if (ParentController.directionModifier == 1)
-        {
-            if (ParentController.AimDirection.x > 0) SetTransform(180, -90, -180-Mathf.Rad2Deg * Mathf.Atan(ParentController.AimDirection.y / -ParentController.AimDirection.x), lerp);
-            else SetTransform(0, -90, Mathf.Rad2Deg * Mathf.Atan(ParentController.AimDirection.y / -ParentController.AimDirection.x), lerp);
-        }
-        else
-        {
-            if (ParentController.AimDirection.x > 0) SetTransform(0, -90, Mathf.Rad2Deg * Mathf.Atan(-ParentController.AimDirection.y / -ParentController.AimDirection.x), lerp);
-            else SetTransform(180, -90, -180 - Mathf.Rad2Deg * Mathf.Atan(-ParentController.AimDirection.y / -ParentController.AimDirection.x), lerp);
-        }       
+        if (ParentController.AimDirection.x < 0) SetTransform(0, 0, -180 - Mathf.Rad2Deg * Mathf.Atan(ParentController.AimDirection.y / -ParentController.AimDirection.x));
+        else SetTransform(0, 0, - Mathf.Rad2Deg * Mathf.Atan(ParentController.AimDirection.y / -ParentController.AimDirection.x));
     }
 
-    private void SetTransform(float x, float y, float z, bool lerp)
+    private void NetSetTransform(Vector3 netAim)
     {
-        if (!lerp)
-        {
-            networkedRotation = new Vector3(x, y, z);
-            transform.position = GunPivot.position;
-            transform.localRotation = Quaternion.Euler(x, y, z);
-        }
-        else
-        {
-            transform.position = Vector3.MoveTowards(transform.position, GunPivot.position, 1.0f / PhotonNetwork.SerializationRate);
-            Quaternion desiredRotation = Quaternion.Euler(x, y, z);
-            float angle = Quaternion.Angle(transform.rotation, desiredRotation);
-            transform.localRotation = Quaternion.RotateTowards(Quaternion.Euler(transform.localRotation.eulerAngles), desiredRotation, angle * (1.0f / PhotonNetwork.SerializationRate));
-        }
+        float speed = 15;
+        transform.localRotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(netAim), speed * Time.deltaTime);
+    }
+
+    private void SetTransform(float x, float y, float z)
+    {
+        networkedRotation = new Vector3(x, y, z);
+        transform.position = GunPivot.position;
+        transform.rotation = Quaternion.Euler(x, y, z);
     }
 
     public virtual void Attack(Vector3 Direction)
